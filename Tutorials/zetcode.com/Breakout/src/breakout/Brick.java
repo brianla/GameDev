181f42b472a5aea1a3ffcf17d95ac6f639cfd564
brianla <brianla408@gmail.com>
2013-12-19 12:38:03 -0800

    Java2D Game Tutorial - Breakout

---
+++ Tutorials/zetcode.com/Breakout/src/breakout/Brick.java
@@ -0,0 +1,30 @@
+package breakout;
+
+import javax.swing.ImageIcon;
+
+public class Brick extends Sprite {
+
+	String brickie = "../images/brickie.png";
+	boolean destroyed;
+	
+	public Brick(int x, int y) {
+		this.x = x;
+		this.y = y;
+		
+		ImageIcon ii = new ImageIcon(this.getClass().getResource(brickie));
+		image = ii.getImage();
+		
+		width = image.getWidth(null);
+		height = image.getHeight(null);
+		
+		destroyed = false;
+	}
+	
+	public boolean isDestroyed() {
+		return destroyed;
+	}
+	
+	public void setDestroyed(boolean destroyed) {
+		this.destroyed = destroyed;
+	}
+ }
