4498885f92305f0a71e428ce8c05e03f7a1ceb34
DBOOGA <nguyend0703@yahoo.com>
2013-12-19 22:14:17 -0800

    Updates day 1
    Lives
    Timer
    PowerUp progress

---
+++ Projects/Breakout/src/breakout/Breakout.java
@@ -0,0 +1,30 @@
+package breakout;
+
+import java.awt.BorderLayout;
+import java.awt.FlowLayout;
+
+import javax.swing.ImageIcon;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+
+public class Breakout extends JFrame {
+
+    public Breakout()
+    {
+        add(new Board());
+        setTitle("Breakout");
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        setSize(Commons.WIDTH, Commons.HEIGHT);
+        setLocationRelativeTo(null);
+        setIgnoreRepaint(true);
+        setResizable(false);
+        setVisible(true);
+        
+
+        
+    }
+
+    public static void main(String[] args) {
+        new Breakout();
+    }
+}
---
+++ Projects/Breakout/src/breakout/Item.java
@@ -0,0 +1,56 @@
+package breakout;
+
+import javax.swing.ImageIcon;
+
+public class Item extends Sprite implements Commons {
+	   private int ydir;
+
+	   protected String item = "block1.jpg";
+	   boolean destroyed;
+
+	   public Item(int x, int y) {
+		 this.x = x;
+		 this.y = y;
+	     ydir = 1;
+
+	     ImageIcon ii = new ImageIcon(this.getClass().getResource(item));
+	     image = ii.getImage();
+
+	     width = image.getWidth(null);
+	     heigth = image.getHeight(null);
+
+	    }
+
+
+	    public void move()
+	    {
+	      y += ydir;
+
+	      if (y == HEIGHT) {
+	        setYDir(0);
+	        setDestroyed(true);
+	      }
+	      
+	    }
+
+	    public void setYDir(int y)
+	    {
+	      ydir = y;
+	    }
+
+	    public int getYDir()
+	    {
+	      return ydir;
+	    }
+	    
+	    public boolean isDestroyed()
+	    {
+	      return destroyed;
+	    }
+
+	    public void setDestroyed(boolean destroyed)
+	    {
+	      this.destroyed = destroyed;
+	    }
+
+}
