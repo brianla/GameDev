df077f9141078bd1b6730f85a17c6691521fa662
DBOOGA <nguyend0703@yahoo.com>
2013-12-23 20:16:16 -0800

    Day 1 - 2D Platformer

---
+++ Projects/Undecided/Assets/Scripts/PlayerScript.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerScript : MonoBehaviour {
+
+    private bool isGrounded = false;
+    private Vector2 movement, jumpMovement;
+    public float jumpHeight = 1000f;
+    public float speed = 8.0f;
+	
+	// Update is called once per frame
+	void Update () {
+        if(Input.GetButtonDown("Jump") && isGrounded){
+            Jump();
+        }
+	}
+
+    void FixedUpdate()
+    {
+        float inputX = Input.GetAxis("Horizontal");
+
+        //isGrounded = Physics2D.Raycast(transform.position, -Vector2.up, .4f);
+        isGrounded = (rigidbody2D.velocity.y == 0);
+
+        transform.position += transform.right * inputX * speed * Time.deltaTime;
+    }
+
+    void Jump()
+    {
+        if (!isGrounded)
+        {
+            return;
+        }
+        rigidbody2D.AddForce(transform.up * jumpHeight);
+    }
+}
