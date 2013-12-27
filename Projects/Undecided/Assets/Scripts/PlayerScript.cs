41d4b924f866a68969ef07bae36dba583a1924bb
brianla <brianla408@gmail.com>
2013-12-27 14:44:09 -0800

    Fixed Jump and Added Camera Movement

--- Projects/Undecided/Assets/Scripts/PlayerScript.cs
+++ Projects/Undecided/Assets/Scripts/PlayerScript.cs
@@ -7,13 +7,6 @@ public class PlayerScript : MonoBehaviour {
     private Vector2 movement, jumpMovement;
     public float jumpHeight = 1000f;
     public float speed = 8.0f;
-	
-	// Update is called once per frame
-	void Update () {
-        if(Input.GetButtonDown("Jump") && isGrounded){
-            Jump();
-        }
-	}
 
     void FixedUpdate()
     {
@@ -23,10 +16,15 @@ public class PlayerScript : MonoBehaviour {
         isGrounded = (rigidbody2D.velocity.y == 0);
 
         transform.position += transform.right * inputX * speed * Time.deltaTime;
-    }
 
-    void Jump()
-    {
+// Changed GetButtonMove() to GetButton; moved from Update to FixedUpdate()
+		if(Input.GetButton("Jump") && isGrounded){
+			Jump();
+		}
+	}
+	
+	void Jump()
+	{
         if (!isGrounded)
         {
             return;
