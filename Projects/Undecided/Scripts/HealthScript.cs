57bc96406c0acf7fe193679d158d96615f33c28d
brianla <brianla408@gmail.com>
2013-12-31 16:19:40 -0800

    Test Sync 2

---
+++ Projects/Undecided/Scripts/HealthScript.cs
@@ -0,0 +1,106 @@
+﻿using UnityEngine;
+
+/// <summary>
+/// Handle hitpoints and damages
+/// </summary>
+public class HealthScript : MonoBehaviour
+{
+    /// <summary>
+    /// Total hitpoints
+    /// </summary>
+    public int hp = 2;
+
+    /// <summary>
+    /// Enemy or player?
+    /// </summary>
+    public bool isEnemy = true;
+
+
+
+// Knockback distance
+	public float knockbackX = 100f;
+	public float knockbackY = 1000f;
+	
+	public float invuln = 2f;
+	public float invAlpha = 0.5f;
+
+	private bool playerHit = false;
+	private float timeHit;
+
+
+
+    void OnTriggerEnter2D(Collider2D collider)
+    {
+        // Is this a shot?
+        ShotScript shot = collider.gameObject.GetComponent<ShotScript>();
+        if (shot != null)
+        {
+            // Avoid friendly fire
+            if (shot.isEnemyShot != isEnemy)
+            {
+				// Destroy the shot
+				// Remember to always target the game object,
+				// otherwise you will just remove the script.
+				Destroy(shot.gameObject);
+
+				if (isEnemy || (!isEnemy && !playerHit)) {
+	                hp -= shot.damage;
+
+	                if (hp <= 0)
+	                {
+
+	                    // 'Splosion!
+	                  //  SpecialEffectsHelper.Instance.Explosion(transform.position);
+	                  //  SoundEffectsHelper.Instance.MakeExplosionSound();
+
+	                    // Dead!
+	                    Destroy(gameObject);
+					}
+
+	// Knockback effect to Player
+					if (!isEnemy) {
+						
+						playerHit = true;
+						timeHit = Time.time;
+						
+						// If shot is moving left, then knockback to the left
+						if (shot.rigidbody2D.velocity.x < 0)
+							Knockback(false);
+						else
+							Knockback(true);
+					}
+				}
+            }
+        }
+    }
+
+	void Knockback(bool isRight) {
+		Vector3 horizontalKB = transform.right;
+		
+		if (!isRight) {
+			horizontalKB *= -1;
+		}
+		
+		this.rigidbody2D.AddForce((horizontalKB * knockbackX) + (transform.up * knockbackY));
+	}
+
+	void Invulnerable() {
+		SpriteRenderer renderer = this.gameObject.GetComponent<SpriteRenderer>();
+
+		if ( (((Time.time - timeHit) * 100) %  50) < 25) {
+			renderer.color = new Color(1f, 1f, 1f, invAlpha);
+		}
+		else {
+			renderer.color = new Color(1f, 1f, 1f, 1f);
+		}
+	}
+
+	void Update() {
+		if (playerHit && (Time.time - timeHit) >= invuln) {
+			playerHit = false;
+		}
+		else if (playerHit) {
+			Invulnerable();
+		}
+	}
+}
---
+++ Projects/Undecided/Scripts/PlayerScript.cs
@@ -0,0 +1,71 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerScript : MonoBehaviour {
+
+    private bool isGrounded = false;
+    private Vector2 movement, jumpMovement;
+    public float jumpHeight = 1000f;
+    public float speed = 8.0f;
+    private bool facingRight = true;
+
+    void FixedUpdate()
+    {
+        float inputX = Input.GetAxis("Horizontal");
+
+        isGrounded = (rigidbody2D.velocity.y == 0);
+
+        transform.position += transform.right * inputX * speed * Time.deltaTime;
+
+		if(Input.GetButton("Jump") && isGrounded){
+			Jump();
+		}
+
+        if (inputX > 0 && !facingRight)
+            Flip();
+        else if (inputX < 0 && facingRight)
+            Flip();
+
+	}
+
+    void Update()
+    {
+        bool shoot = Input.GetButtonDown("Fire1");
+        shoot |= Input.GetButtonDown("Fire2");
+
+        if (shoot)
+        {
+            WeaponScript weapon = GetComponent<WeaponScript>();
+            if (weapon != null)
+            {
+                // false because the player is not an enemy
+                weapon.Attack(false);
+              //  SoundEffectsHelper.Instance.MakePlayerShotSound();
+            }
+        }
+    }
+	
+	void Jump()
+	{
+        if (!isGrounded)
+        {
+            return;
+        }
+        rigidbody2D.AddForce(transform.up * jumpHeight);
+    }
+
+    void Flip()
+    {
+
+        facingRight = !facingRight;
+
+        Vector3 theScale = transform.localScale;
+        theScale.x *= -1;
+        transform.localScale = theScale;
+    }
+
+	public bool Grounded()
+	{
+		return isGrounded;
+	}
+}
